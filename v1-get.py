bdda599df8c89bcb37b243275b02ffab33ef3f1e
tani <dratani@gmail.com>
2021-09-07 12:05:33 -0500

    Initial commit

---
+++ v1-get.py
@@ -0,0 +1,38 @@
+"""
+SNMPv1
+++++++
+
+Send SNMP GET request using the following options:
+
+  * with SNMPv1, community 'comunidadASR'
+  * over IPv4/UDP
+  * to an Agent at localhost
+  * for two instances of SNMPv2-MIB::sysDescr.0 MIB object,
+
+Functionally similar to:
+
+| $ snmpget -v1 -c comunidadASR localhost 1.3.6.1.2.1.1.1.0
+
+"""#
+from pysnmp.hlapi import *
+
+iterator = getCmd(
+    SnmpEngine(),
+    CommunityData('comunidadASR', mpModel=0),
+    UdpTransportTarget(('localhost', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0'))
+)
+
+errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+if errorIndication:
+    print(errorIndication)
+
+elif errorStatus:
+    print('%s at %s' % (errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+else:
+    for varBind in varBinds:
+        print(' = '.join([x.prettyPrint() for x in varBind]))
