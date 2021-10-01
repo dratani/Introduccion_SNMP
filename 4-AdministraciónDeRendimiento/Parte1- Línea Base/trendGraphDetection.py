381b48bf095caf15617f1020bbd177ce5fc2403e
tani <dratani@gmail.com>
2021-10-01 13:55:16 -0500

    Notify

--- 4-AdministraciónDeRendimiento/Parte1- Línea Base/trendGraphDetection.py
+++ 4-AdministraciónDeRendimiento/Parte1- Línea Base/trendGraphDetection.py
@@ -1,6 +1,6 @@
 import sys
 import rrdtool
-#from AdministracionDeRendimiento.Parte3.Notify import send_alert_attached
+from  Notify import send_alert_attached
 import time
 rrdpath = '/home/tani/PycharmProjects/Introduccion_SNMP/4-AdministraciónDeRendimiento/RRD/'
 imgpath = '/home/tani/PycharmProjects/Introduccion_SNMP/4-AdministraciónDeRendimiento/IMG/'
@@ -17,16 +17,18 @@ ret = rrdtool.graphv( imgpath+"deteccion.png",
                     '--upper-limit', '100',
                     "--title=Uso del CPU del agente Usando SNMP y RRDtools \n Detección de umbrales",
 
-                      "DEF:cargaCPU="+rrdpath+"trend.rrd:CPUload:AVERAGE",
+                    "DEF:cargaCPU="+rrdpath+"trend.rrd:CPUload:AVERAGE",
 
-                     "CDEF:umbral5=cargaCPU,5,LT,0,cargaCPU,IF",
                      "VDEF:cargaMAX=cargaCPU,MAXIMUM",
                      "VDEF:cargaMIN=cargaCPU,MINIMUM",
                      "VDEF:cargaSTDEV=cargaCPU,STDEV",
                      "VDEF:cargaLAST=cargaCPU,LAST",
+
+                     "CDEF:umbral5=cargaCPU,15,LT,0,cargaCPU,IF",
                      "AREA:cargaCPU#00FF00:Carga del CPU",
                      "AREA:umbral5#FF9F00:Carga CPU mayor que 5",
-                     "HRULE:5#FF0000:Umbral 1 - 5%",
+                     "HRULE:15#FF0000:Umbral 1 - 5%",
+
                      "PRINT:cargaLAST:%6.2lf",
                      "GPRINT:cargaMIN:%6.2lf %SMIN",
                      "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
@@ -35,5 +37,5 @@ print (ret)
 
 ultimo_valor=float(ret['print[0]'])
 if ultimo_valor>4:
-    #send_alert_attached("Sobrepasa Umbral línea base")
+    send_alert_attached("Sobrepasa Umbral línea base")
     print("Sobrepasa Umbral línea base")
