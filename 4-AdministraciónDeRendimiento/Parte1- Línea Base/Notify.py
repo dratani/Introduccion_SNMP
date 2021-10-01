1331da1331549493fa3f0fb7ca235c36d2e4f1e4
tani <dratani@gmail.com>
2021-10-01 12:57:45 -0500

    Notify

---
+++ 4-AdministraciónDeRendimiento/Parte1- Línea Base/Notify.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+COMMASPACE = ', '
+# Define params
+rrdpath = '/home/tani/PycharmProjects/Introduccion_SNMP/4-AdministraciónDeRendimiento/RRD/'
+imgpath = '/home/tani/PycharmProjects/Introduccion_SNMP/4-AdministraciónDeRendimiento/IMG/'
+fname = 'trend.rrd'
+
+mailsender = "dummycuenta3@gmail.com"
+mailreceip = "dummycuenta3@gmail.com"
+mailserver = 'smtp.gmail.com: 587'
+password = 'Secreto123!'
+
+def send_alert_attached(subject):
+    """ Envía un correo electrónico adjuntando la imagen en IMG
+    """
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = mailsender
+    msg['To'] = mailreceip
+    fp = open(imgpath+'deteccion.png', 'rb')
+    img = MIMEImage(fp.read())
+    fp.close()
+    msg.attach(img)
+    s = smtplib.SMTP(mailserver)
+
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(mailsender, password)
+
+    s.sendmail(mailsender, mailreceip, msg.as_string())
+    s.quit()
