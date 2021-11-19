ea8a15b38f855cd41972617e8bf7340164bf9208
tani <dratani@gmail.com>
2021-11-19 12:21:23 -0600

    Trap

---
+++ 5.AdministraciónDeFallas/Notification2.py
@@ -0,0 +1,14 @@
+import sys
+
+host = input()
+ip = input()
+varbind = []
+while 1:
+    try:
+        varbind.append(input())
+    except EOFError:
+        print("Fin de lectura")
+        break
+
+print("Datos obtenidos desde python")
+print(host, ip, varbind)
