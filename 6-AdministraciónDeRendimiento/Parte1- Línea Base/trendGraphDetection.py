94855c09a5881bc78a7fa4e65f474020654c763c
tani <dratani@gmail.com>
2022-11-14 11:40:47 -0600

    graph detection

--- 6-AdministraciónDeRendimiento/Parte1- Línea Base/trendGraphDetection.py
+++ 6-AdministraciónDeRendimiento/Parte1- Línea Base/trendGraphDetection.py
@@ -1,15 +1,16 @@
 import sys
 import rrdtool
+import time
 from  Notify import send_alert_attached
 import time
 rrdpath = '/home/tani/PycharmProjects/Introduccion_SNMP/6-AdministraciónDeRendimiento/RRD/'
 imgpath = '/home/tani/PycharmProjects/Introduccion_SNMP/6-AdministraciónDeRendimiento/IMG/'
+ultima_actualizacion = int(rrdtool.last(rrdpath + "trend.rrd"))
 
-ultima_lectura = int(rrdtool.last(rrdpath+"trend.rrd"))
-tiempo_final = ultima_lectura
-tiempo_inicial = tiempo_final - 1800
-
-ret = rrdtool.graphv( imgpath+"deteccion.png",
+def generarGrafica(ultima_lectura):
+    tiempo_final = int(ultima_lectura)
+    tiempo_inicial = tiempo_final - 1800
+    ret = rrdtool.graphv( imgpath+"deteccion.png",
                      "--start",str(tiempo_inicial),
                      "--end",str(tiempo_final),
                      "--vertical-label=Cpu load",
@@ -21,18 +22,19 @@ ret = rrdtool.graphv( imgpath+"deteccion.png",
                      "VDEF:cargaMIN=cargaCPU,MINIMUM",
                      "VDEF:cargaSTDEV=cargaCPU,STDEV",
                      "VDEF:cargaLAST=cargaCPU,LAST",
-                 #   "CDEF:cargaEscalada=cargaCPU,8,*",
-                     "CDEF:umbral10=cargaCPU,20,LT,0,cargaCPU,IF",
+                     "CDEF:umbral5=cargaCPU,5,LT,0,cargaCPU,IF",
                      "AREA:cargaCPU#00FF00:Carga del CPU",
-                     "AREA:umbral10#FF9F00:Carga CPU mayor que 10",
-                     "HRULE:20#FF0000:Umbral 10 - 5%",
+                     "AREA:umbral5#FF9F00:Carga CPU mayor que 5",
+                     "HRULE:5#FF0000:Umbral  5%",
                      "PRINT:cargaLAST:%6.2lf",
                      "GPRINT:cargaMIN:%6.2lf %SMIN",
                      "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
                      "GPRINT:cargaLAST:%6.2lf %SLAST" )
-print (ret)
+    print (ret)
 
-ultimo_valor=float(ret['print[0]'])
-if ultimo_valor>10:
-    send_alert_attached("Sobrepasa Umbral línea base")
-    print("Sobrepasa Umbral línea base")
+while (1):
+    if ultima_actualizacion > 5:
+        generarGrafica(ultima_actualizacion)
+        send_alert_attached("Sobrepasa el umbral")
+        print("sobrepasa el umbral")
+    time.sleep(20)
