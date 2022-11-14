bf2b38e0138533aa220ccc57c4c5b92c3fcba8b3
tani <dratani@gmail.com>
2022-11-14 11:42:41 -0600

    graph detection

--- 6-AdministraciónDeRendimiento/Parte1- Línea Base/TrendUpdate.py
+++ 6-AdministraciónDeRendimiento/Parte1- Línea Base/TrendUpdate.py
@@ -5,11 +5,10 @@ rrdpath = '/home/tani/PycharmProjects/Introduccion_SNMP/6-AdministraciónDeRendi
 carga_CPU = 0
 
 while 1:
-    carga_CPU = int(consultaSNMP('comunidadASR','localhost','1.3.6.1.2.1.25.3.3.1.2.196608'))
+    carga_CPU = int(consultaSNMP('comunidadSNMP','localhost','1.3.6.1.2.1.25.3.3.1.2.196608'))
     valor = "N:" + str(carga_CPU)
     print (valor)
     rrdtool.update(rrdpath+'trend.rrd', valor)
-   # rrdtool.dump(rrdpath+'trend.rrd','trend.xml')
     time.sleep(5)
 
 if ret:
